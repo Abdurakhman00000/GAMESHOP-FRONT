a8cda6fd7e5d376a16a675ef0a3629f8618da9b1
rahmanovdev <jumadilrahmonov@gmail.com>
2025-02-15 03:03:25 +0600

    optimization subhero

--- src/components/pages/subscribe/Sub-hero/SubHeroPage.tsx
+++ src/components/pages/subscribe/Sub-hero/SubHeroPage.tsx
@@ -1,44 +1,75 @@
-import React from 'react'
-import scss from "./SubHeroPage.module.scss"
-import gameConsole from "../../../../../public/images/heroSub-imgs/cool_img2.svg";
-import gameConsolePlus from "../../../../../public/images/heroSub-imgs/cool_img3.svg";
+import { memo } from 'react';
 import Image from 'next/image';
-import leftImg from "../../../../../public/images/adaptive-hero-img/adap1.svg";
-import rightImg from "../../../../../public/images/adaptive-hero-img/adap2.svg";
+import scss from './SubHeroPage.module.scss';
+import gameConsole from '../../../../../public/images/heroSub-imgs/cool_img2.svg';
+import gameConsolePlus from '../../../../../public/images/heroSub-imgs/cool_img3.svg';
+import leftImg from '../../../../../public/images/adaptive-hero-img/adap1.svg';
+import rightImg from '../../../../../public/images/adaptive-hero-img/adap2.svg';
 
 const SubHeroPage = () => {
-  return (
-    <section className={scss.Main}>
-      <div className={scss.background_img}></div>
-      <div className="container">
-        <div className={scss.content}>
-          <div className={scss.left_content}>
-            <h1>PlayStation Plus</h1>
-            <p>
-              Подписывайтесь на PlayStation Plus и <br /> получайте доступ к
-              играм по лучшим ценам!
-            </p>
-            <button>Подключить подписку</button>
-          </div>
+	return (
+		<section className={scss.Main}>
+			<div className={scss.background_img} />
 
-          <div className={scss.right_content}>
-            <div className={scss.img1}>
-              <Image src={gameConsole} alt="" width={750} height={300} />
-            </div>
-            <div className={scss.img2}>
-              <Image src={gameConsolePlus} alt="" width={750} height={300} />
-            </div>
-          </div>
-        </div>
-      </div>
+			<div className='container'>
+				<div className={scss.content}>
+					<div className={scss.left_content}>
+						<h1>PlayStation Plus</h1>
+						<p>
+							Подписывайтесь на PlayStation Plus и <br /> получайте доступ к играм по
+							лучшим ценам!
+						</p>
+						<button>Подключить подписку</button>
+					</div>
 
-      <div className={scss.adaptive_imgs}>
-        <Image src={leftImg} alt="" width={79} height={79} />
-        <Image src={rightImg} alt="" width={79} height={79} />
-      </div>
+					<div className={scss.right_content}>
+						<div className={scss.img1}>
+							<Image
+								src={gameConsole}
+								alt='PlayStation gaming console'
+								width={750}
+								height={300}
+								quality={70}
+								priority={true}
+								sizes='(max-width: 1007px) 100vw, 700px'
+							/>
+						</div>
+						<div className={scss.img2}>
+							<Image
+								src={gameConsolePlus}
+								alt='PlayStation Plus features'
+								width={750}
+								height={300}
+								quality={70}
+								loading='lazy'
+								sizes='(max-width: 1007px) 100vw, 350px'
+							/>
+						</div>
+					</div>
+				</div>
+			</div>
 
-    </section>
-  );
+			{/* Adaptive Images */}
+			<div className={scss.adaptive_imgs}>
+				<Image
+					src={leftImg}
+					alt='PlayStation adaptive left'
+					width={79}
+					height={79}
+					loading='lazy'
+					quality={70}
+				/>
+				<Image
+					src={rightImg}
+					alt='PlayStation adaptive right'
+					width={79}
+					height={79}
+					loading='lazy'
+					quality={70}
+				/>
+			</div>
+		</section>
+	);
 };
 
-export default SubHeroPage;
+export default memo(SubHeroPage);
