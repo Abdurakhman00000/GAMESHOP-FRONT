449c68b21945246d90ab3bf661f41c6dee3501f3
Arsenoverkill <arsenprogrammer007@gmail.com>
2025-04-21 23:12:51 +0600

    subs service second

--- src/redux/api/subs-data/index.ts
+++ src/redux/api/subs-data/index.ts
@@ -1,20 +1,35 @@
-import {api as index} from "..";
+import { api as index } from "..";
 import { SUBS } from "./types";
 
 const api = index.injectEndpoints({
-    endpoints: (build) => ({
-        subsServices: build.query<SUBS.GetSubsServicesResponse, SUBS.GetSubsServicesRequest>({
-            query: () => ({
-                url: `/subscription-services`,
-                method: "GET",
-                headers: {
-                    'Content-Type': 'application/json'
-                }
-            }),
-            providesTags: ["subs"]
-        })
-    })
-})
+  endpoints: (build) => ({
+    subsServices: build.query<
+      SUBS.GetSubsServicesResponse,
+      SUBS.GetSubsServicesRequest
+    >({
+      query: () => ({
+        url: `/subscription-services`,
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }),
+      providesTags: ["subsService"],
+    }),
+    getSubscribe: build.query<
+      SUBS.GetSubsServicesResponse,
+      SUBS.GetSubsServicesRequest
+    >({
+      query: () => ({
+        url: `/subscriptions`,
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }),
+      providesTags: ["subscribe"],
+    }),
+  }),
+});
 
-
-export const { useSubsServicesQuery } = api;
+export const { useSubsServicesQuery, useGetSubscribeQuery } = api;
--- src/redux/api/subs-data/types.d.ts
+++ src/redux/api/subs-data/types.d.ts
@@ -1,20 +1,19 @@
 export namespace SUBS {
-    type GetSubsServicesResponse = {
-      id: number;
-      name: string;
-      consoles: number[];
-      choices_level: string;
-      image: string;
-      contents: {
-        title: string;
-        icon: string;
-      }[];
-      periods: {
-        months: number;
-        price: string;
-      }[];
-    }[];
-  
-    type GetSubsServicesRequest = void;
-  }
-  
+  type GetSubsServicesResponse = {
+    id: number;
+    name: string;
+    consoles: Array<number>;
+    choices_level: string;
+    image: string;
+    contents: Array<{
+      title: string;
+      icon: string;
+    }>;
+    periods: Array<{
+      months: number;
+      price: string;
+    }>;
+  }[];
+
+  type GetSubsServicesRequest = void;
+}
