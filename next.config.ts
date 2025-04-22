3aad188324c75b62096b95c9d897c40c492917ab
Abdurakhman <abdibaitovabdurahman@gmail.com>
2025-04-22 15:25:22 +0600

    bugFix: fix

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,9 @@ const nextConfig: NextConfig = {
   images: {
     domains: ["psgamezz.ru"],
   },
+  env: {
+    NEXT_PUBLIC_DATABASE_API: process.env.NEXT_PUBLIC_DATABASE_API,
+  },
 };
 
 export default nextConfig;
